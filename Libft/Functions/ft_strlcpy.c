96c4cac9d2122c22cf15e055a9d5fa8b2733f5fa
Andreas Theodorus Deters <adeters@c3r7p3.42vienna.com>
2024-09-03 11:57:01 +0200

    maybe fixed the strlcpy function

--- Libft/Functions/ft_strlcpy.c
+++ Libft/Functions/ft_strlcpy.c
@@ -10,32 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlcpy(char *dest, char *src, int size)
+#include <stdlib.h>
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	int	i;
+	int	y;
 
 	i = 0;
-	while (src[i])
+	y = 0;
+	while (src[y])
+		y++;
+	if (size > 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	dest[size] = '\0';
-	return (i);
+	return (y);
 }
-
+/*
 #include <stdio.h>
 #include <bsd/string.h>
 
 int	main(void)
 {
 	char dest1[10];
-	char src1[15] = "Hellooooooo";
+	char src1[20] = "worldHellooooooo";
 	char dest2[10];
-	char src2[15] = "Hellooooooo";
+	char src2[20] = "worldHellooooooo";
 
-	printf("%i\n", ft_strlcpy(dest1, src1, 11));
-	printf("%s\n", src1);
-	printf("%zu\n", strlcpy(dest2, src2, 11));
-	printf("%s\n", src2);
-}
+	printf("The result of your function is:\n");
+	printf("%zu\n", ft_strlcpy(dest1, src1, 10));
+	printf("Original string: %s\n", src1);
+	printf("Copied string: %s\n", dest1);
+	printf("The result of the original function is:\n");
+	printf("%zu\n", strlcpy(dest2, src2, 10));
+	printf("Original String: %s\n", src2);
+	printf("Copied String: %s\n", dest2);
+}*/

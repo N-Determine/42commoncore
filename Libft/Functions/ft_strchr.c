71e3e8b2495dc026f45498386dcf2ea5d866cf67
Andreas Theodorus Deters <adeters@c3r6p2.42vienna.com>
2024-09-04 13:13:28 +0200

    added strchr

---
+++ Libft/Functions/ft_strchr.c
@@ -0,0 +1,25 @@
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (char *)&s[i];
+		i++;
+	}
+	if (s[i] == c)
+		return (char *)&s[i];
+	return (0);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	char *s = "Hello WorlWd!";
+	printf("%s\n", ft_strchr(s, 'W'));
+	printf("%s\n", strchr(s, 'W'));
+}

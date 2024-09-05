d76d88b8e67936fe7664a0e3ebdb67a61a87bd1a
Andreas Theodorus Deters <adeters@c3r6p2.42vienna.com>
2024-09-05 13:20:18 +0200

    added strjoin

--- Libft/Functions/ft_strjoin.c
+++ Libft/Functions/ft_strjoin.c
@@ -1,39 +1,61 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/05 13:07:29 by adeters           #+#    #+#             */
-/*   Updated: 2024/09/05 13:19:03 by adeters          ###   ########.fr       */
+/*   Created: 2024/09/05 13:02:59 by adeters           #+#    #+#             */
+/*   Updated: 2024/09/05 13:13:54 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+int	ft_strlen(char *str);
+
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*ptr;
+	int		size;
 	int		i;
+	int		j;
+	char	*ptr;
 
 	i = 0;
-	ptr = (char *)malloc((len + 1) * sizeof(char));
+	j = 0;
+	size = ft_strlen ((char *)s1) + ft_strlen ((char *)s2) + 1;
+	ptr = (char *)malloc (size * sizeof(char));
 	if (ptr == 0)
 		return (0);
-	while (i < len)
+	while (s1[i])
 	{
-		ptr[i] = s[start + i];
+		ptr[i] = s1[i];
 		i++;
 	}
-	ptr[i] = '\0';
+	while (s2[j])
+	{
+		ptr[i + j] = s2[j];
+		j++;
+	}
+	ptr[i + j] = '\0';
 	return (ptr);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 /*
 #include <stdio.h>
 
 int	main(void)
 {
-	char *s = "Ernährungsexperte";
-	printf("%s\n", ft_substr(s, 10, 3));
+	char *s1 = "Hello";
+	char *s2 = " World!";
+	printf("%s\n", ft_strjoin(s1, s2));
 }*/

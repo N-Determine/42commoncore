96ec3e5071e71233b4d4d50e2be550b4e5ff4fe2
Andreas Theodorus Deters <adeters@c3r6p2.42vienna.com>
2024-09-07 15:29:17 +0200

    added calloc

---
+++ Libft/Functions/ft_calloc.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/07 14:49:09 by adeters           #+#    #+#             */
+/*   Updated: 2024/09/07 15:28:55 by adeters          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <limits.h>
+#include <stdint.h>
+
+void	ft_bzero(void *s, size_t n);
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	if (size != 0 && nmemb > SIZE_MAX / size)
+		return (0);
+	if (nmemb * size <= 0)
+		return (0);
+	ptr = malloc(nmemb * size);
+	if (ptr == 0)
+		return (0);
+	ft_bzero(ptr, nmemb * size);
+	return (ptr);
+}
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	char *ptr = (char *)ft_calloc(5,2);
+	printf("%s\n", ptr);
+}*/

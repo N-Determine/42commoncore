1d1b1adeb6aa96e7a0bcba42bfc9e80d4d782edd
Andreas Theodorus Deters <adeters@c3r6p2.42vienna.com>
2024-09-07 17:01:07 +0200

    added first (non working) version of ft_strmapi.c

---
+++ Libft/Functions/ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/07 16:08:58 by adeters           #+#    #+#             */
+/*   Updated: 2024/09/07 16:57:38 by adeters          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str);
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char const	*ptr;
+	int			i;
+	int			len;
+
+	i = 0;
+	len = ft_strlen((char *)s);
+	ptr = malloc(len + 1);
+	if (ptr == 0)
+		return (0);
+	while (i < len)
+	{
+		ptr[i] = f(i, s[i]);
+		i++;
+	}
+	return (ptr);
+}

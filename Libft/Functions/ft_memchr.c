b1be047fb6690cd4999e5cd155573c625df246a3
Andreas Theodorus Deters <adeters@c3r6p2.42vienna.com>
2024-09-07 13:00:06 +0200

    added ft_memchr

---
+++ Libft/Functions/ft_memchr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/07 12:54:07 by adeters           #+#    #+#             */
+/*   Updated: 2024/09/07 12:59:49 by adeters          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	int				i;
+	unsigned char	*ptr;
+
+	i = 0;
+	ptr = (unsigned char *)s;
+	while (i < n)
+	{
+		if (ptr[i] == c)
+			return ((void *)&s[i]);
+		i++;
+	}
+	return (0);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char *s = "Hello World!";
+	char *ptr = ft_memchr(s, 'l', 10);
+	printf("%s\n", ptr);
+}*/

abd66850e3035ed7f6f004dc4e88a4781bc1d118
Andreas Theodorus Deters <adeters@c3r6p2.42vienna.com>
2024-09-08 19:39:23 +0200

    deleted the null result for a size or nmemb of 0 in ft_calloc

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 14:49:09 by adeters           #+#    #+#             */
-/*   Updated: 2024/09/08 16:26:36 by adeters          ###   ########.fr       */
+/*   Updated: 2024/09/08 19:38:55 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 
 	if (size != 0 && nmemb > SIZE_MAX / size)
 		return (0);
-	if (nmemb * size <= 0)
-		return (0);
+	//if (nmemb * size <= 0)
+	//	return (0);
 	ptr = malloc(nmemb * size);
 	if (ptr == 0)
 		return (0);

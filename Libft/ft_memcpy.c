66c6b24e16dcc674807b87b77953febfb411f863
N-Determine <73752640+N-Determine@users.noreply.github.com>
2024-09-09 11:23:09 +0000

    only getting rid of segfault if dest & src are 0 in memcpy

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -18,7 +18,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*src_ptr;
 	size_t			i;
 
-	if (dest == (void *)0 || src == (void *)0)
+	if (dest == (void *)0 && src == (void *)0)
 		return (dest);
 	dest_ptr = (unsigned char *)dest;
 	src_ptr = (unsigned char *)src;

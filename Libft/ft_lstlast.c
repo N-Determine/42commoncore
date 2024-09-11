c18f3aa3b977856efd6c0bd948e966957b5f8457
Andreas Theodorus Deters <adeters@c3r6p2.42vienna.com>
2024-09-11 11:27:46 +0200

    added the lstlast function

---
+++ Libft/ft_lstlast.c
@@ -0,0 +1,32 @@
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list *follow;
+
+	if (lst == 0)
+		return (lst);
+	follow = lst->next;
+	while (follow->next != 0)
+	{
+		follow = follow->next;
+	}
+	return (follow);
+}
+/* 
+#include <stdio.h>
+
+int	main(void)
+{
+	char *text = "Hello";
+	char *text1 = " World!";
+	char *text2 = "Blyat";
+	t_list *node = ft_lstnew(text);
+	t_list *node1 = ft_lstnew(text1);
+	t_list *node2 = ft_lstnew(text2);
+	ft_lstadd_front(&node, node1);
+	ft_lstadd_front(&node1, node2);
+	t_list *last = ft_lstlast(node2);
+	printf("%p\n", last);
+	printf("%p\n", node);
+} */

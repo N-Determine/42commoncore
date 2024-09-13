e44bbb89da5300dae93e7c950f7a7d6291d9fcbb
Andreas Theodorus Deters <adeters@c3r7p2.42vienna.com>
2024-09-13 14:15:51 +0200

    protecting against non existing new node in lstaddback

--- Libft/ft_lstadd_back_bonus.c
+++ Libft/ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 12:25:51 by adeters           #+#    #+#             */
-/*   Updated: 2024/09/13 14:10:03 by adeters          ###   ########.fr       */
+/*   Updated: 2024/09/13 14:15:29 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	t_list	*last;
 
 	last = ft_lstlast(*lst);
-	if (last != 0)
+	if (last != 0 && new != 0)
 	{
 		last->next = new;
 		new->next = 0;

0ed4fe7e6caa2c49f43c50a9208ed3aa0337dca1
Andreas Theodorus Deters <adeters@c3r7p3.42vienna.com>
2024-09-15 17:32:38 +0200

    added documentation to my ft_putstr_fd function in the header

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 17:13:24 by adeters           #+#    #+#             */
-/*   Updated: 2024/09/15 17:13:25 by adeters          ###   ########.fr       */
+/*   Updated: 2024/09/15 17:26:54 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,11 @@
 
 #include "libft.h"
 #include <stdarg.h>
+#include <stdio.h>
 #include <unistd.h>
 
 static int	ft_is_specifier(char c);
-
-static void	ft_var_printer(char code, va_list list)
-{
-	if (code == 's')
-		ft_putstr_fd(va_arg(list, char *), 1);
-	else if (code == 'c')
-		ft_putchar_fd((char)va_arg(list, int), 1);
-	else if (code == 'i' || code == 'd')
-		ft_putnbr_fd(va_arg(list, int), 1);
-}
+static void	ft_var_printer(char code, va_list list);
 
 int	ft_printf(const char *str, ...)
 {
@@ -54,6 +46,15 @@ int	ft_printf(const char *str, ...)
 	return (1);
 }
 
+/**
+ * @brief Checks if the input character is a specifier or % (in contrast to a flag)
+ * 
+ * @param c The character that needs to be checked
+ * 
+ * @return 1 - If the character is a specifier (or %)
+ * 
+ * 			0 - If the charater is no a specifier (or %)
+ */
 static int	ft_is_specifier(char c)
 {
 	if (c == 'c')
@@ -75,6 +76,25 @@ static int	ft_is_specifier(char c)
 	if (c == '%')
 		return (1);
 	return (0);
+}
+
+/**
+ * @brief Prints the content of the list using a differnt function to print it 
+ * depending of the type of content it contains
+ * 
+ * @param code The specifier that decides what function needs to be used to
+ * print the content
+ * 
+ * @param list The pointer to the va_list in which the content needs to be printed
+ */
+static void	ft_var_printer(char code, va_list list)
+{
+	if (code == 's')
+		ft_putstr_fd(va_arg(list, char *), 1);
+	else if (code == 'c')
+		ft_putchar_fd((char)va_arg(list, int), 1);
+	else if (code == 'i' || code == 'd')
+		ft_putnbr_fd(va_arg(list, int), 1);
 }
 
 int	main(void)

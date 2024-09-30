5ab6a5c596252d9eea9bff78eecc925add815a24
Andreas Theodorus Deters <adeters@c2r5p2.42vienna.com>
2024-09-30 19:30:50 +0200

    fixed bug where check_nl returned 0 for fail eventhough it could be a valid index

--- WIP/get_next_line/get_next_line.c
+++ WIP/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 19:32:35 by adeters           #+#    #+#             */
-/*   Updated: 2024/09/29 23:41:51 by adeters          ###   ########.fr       */
+/*   Updated: 2024/09/30 19:28:48 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int	make_line(char *saver, char *line);
+int		make_line(char *saver, char *line);
 
 int	ft_strlen(char *str)
 {
@@ -54,18 +54,18 @@ int	ft_check_nl(char *str)
 
 	index = 0;
 	if (str == NULL)
-		return (0);
+		return (-1);
 	while (str[index])
 	{
 		if (str[index] == '\n')
 			return (index);
 		index++;
 	}
-	return (0);
+	return (-1);
 }
 
 /**
- * Probably very unsafe because it can segfault if dest is to small, 
+ * Probably very unsafe because it can segfault if dest is to small,
  * don't forgett to allocate for \0
  */
 char	*ft_copy_until_char(char *dest, char *src, char c)
@@ -87,9 +87,9 @@ char	*ft_copy_until_char(char *dest, char *src, char c)
 }
 
 /**
- * @brief Updates the static string 'saver' to either contain the 
+ * @brief Updates the static string 'saver' to either contain the
  * new buffer if there is no newline
- * or the rest after the newline. 
+ * or the rest after the newline.
  * It free's the saver everytime before overwritting it.
  */
 char	*ft_update_saver(char *buf, char *old_saver)
@@ -101,14 +101,14 @@ char	*ft_update_saver(char *buf, char *old_saver)
 	free(old_saver);
 	size = 0;
 	nl_pos = ft_check_nl(buf);
-	if (nl_pos == 0)
+	if (nl_pos == -1)
 		size = ft_strlen(buf) + 1;
 	else
 		size = ft_strlen(buf + nl_pos + 1) + 1;
 	new_saver = ft_calloc(size, sizeof(char));
 	if (new_saver == NULL)
 		return (NULL);
-	if (nl_pos == 0)
+	if (nl_pos == -1)
 		new_saver = ft_copy_until_char(new_saver, buf, '\0');
 	else
 		new_saver = ft_copy_until_char(new_saver, (buf + nl_pos + 1), '\0');
@@ -116,20 +116,20 @@ char	*ft_update_saver(char *buf, char *old_saver)
 	return (new_saver);
 }
 
-int		ft_line_len(char *buffer)
+int	ft_line_len(char *buffer)
 {
-	if (ft_check_nl(buffer))
-		return (ft_check_nl(buffer) + 1);
-	else
+	if (ft_check_nl(buffer) == -1)
 		return (ft_strlen(buffer));
+	else
+		return (ft_check_nl(buffer) + 1);
 }
 
 char	*get_next_line(int fd)
 {
-	char			*buffer;
-	static char		*saver;
-	char			*line;
-	size_t			isn_eof;
+	char		*buffer;
+	static char	*saver = NULL;
+	char		*line;
+	size_t		isn_eof;
 
 	if (fd < 0)
 		return (NULL);
@@ -138,21 +138,31 @@ char	*get_next_line(int fd)
 	{
 		buffer = ft_calloc(sizeof(char), BUFFER_SIZE + 1 + ft_strlen(saver));
 		if (buffer == NULL)
+		{
+			free(saver);
 			return (NULL);
+		}
 		ft_copy_until_char(buffer, saver, '\0');
 		isn_eof = read(fd, buffer + ft_strlen(saver), BUFFER_SIZE);
 		if (isn_eof < 0)
 		{
 			free(buffer);
+			free(saver);
 			return (NULL);
 		}
-		if (ft_check_nl(buffer))
+		if (ft_check_nl(buffer) != -1)
 		{
 			line = ft_calloc(sizeof(char), ft_line_len(buffer) + 1);
 			if (line == NULL)
+			{
+				free(buffer);
+				free(saver);
 				return (NULL);
+			}
 			line = ft_copy_until_char(line, buffer, '\n');
 			saver = ft_update_saver(buffer, saver);
+			printf("Check");
+			fflush(stdout);
 			return (line);
 		}
 		saver = ft_update_saver(buffer, saver);
@@ -161,47 +171,94 @@ char	*get_next_line(int fd)
 	{
 		line = ft_calloc(sizeof(char), ft_line_len(saver) + 1);
 		if (line == NULL)
+		{
+			free(saver);
 			return (NULL);
+		}
 		line = ft_copy_until_char(line, saver, '\0');
 		free(saver);
 		saver = NULL;
 		return (line);
 	}
-	free(saver);
 	return (NULL);
 }
+
+/* int	main(void)
+{
+	int fd = open("./texts/text.txt", O_RDWR);
+	char *str = get_next_line(fd);
+	printf("%s", str);
+	free(str);
+	str = get_next_line(fd);
+	printf("%s", str);
+	free(str);
+	str = get_next_line(fd);
+	printf("%s", str);
+	free(str);
+	str = get_next_line(fd);
+	printf("%s", str);
+	free(str);
+	str = get_next_line(fd);
+	printf("%s", str);
+	free(str);
+	str = get_next_line(fd);
+	printf("%s", str);
+	free(str);
+	while (str)
+	{
+		free(str);
+		str = get_next_line(fd);
+		if (str)
+			printf("%s", str);
+	}
+	close(fd);
+	return (0);
+} */
+
+#include "libft.h"
 
 int	main(void)
 {
 	int i = 0;
-	int lines_per_frame = 10;
-	int frame_line = 1;
+	int lines_per_frame = 30;
+	int frame_line = 0;
 	int frames = 11;
 	int index = 0;
-	int fd = open("./texts/text3.txt", O_RDWR);
-	char *str = get_next_line(fd);
-	printf("%s", str);
-	while (str)
+	int fd = open("./texts/text4.txt", O_RDWR);
+	char *str = NULL;
+	int test;
+	setvbuf(stdout, NULL, _IONBF, 0);
+	do
 	{
 		// Print frames * a single frame
-		while (str && index < frames)
+		while (index < frames)
 		{
 			// Clear screen & Print a single frame
-			printf("\033[H\033[J"); fflush(stdout);
+			// printf("\033[H\033[J"); fflush(stdout);
+			// system("clear");
+			ft_putstr_fd("\033[H\033[J", 1);
+			fflush(stdout);
 			frame_line = 0;
-			while (str && frame_line < lines_per_frame)
+			while (frame_line < lines_per_frame)
 			{
-				free(str);
+				if (str)
+					free(str);
 				str = get_next_line(fd);
 				if (str)
-					printf("%s", str);
+				{
+					ft_putstr_fd(str, 1);
+					fflush(stdout);
+				}
 				frame_line++;
 			}
 			// Delay für 6 fps
-			usleep(166667);
+			printf("%i\n", usleep(166667));
 			index++;
 		}
-	}
+		if (str)
+			free(str);
+		str = NULL;
+	} while (str);
 	free(str);
 	close(fd);
 }

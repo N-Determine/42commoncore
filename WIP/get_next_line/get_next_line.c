8cb20c773fd4f28d7f48e9dd27a0ba136b42a913
Andreas Theodorus Deters <adeters@c3r5p6.42vienna.com>
2024-10-02 15:00:04 +0200

    fixed a bug where buffer was not set to null in the free_foo function

--- WIP/get_next_line/get_next_line.c
+++ WIP/get_next_line/get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 13:12:06 by adeters           #+#    #+#             */
-/*   Updated: 2024/10/02 13:37:42 by adeters          ###   ########.fr       */
+/*   Updated: 2024/10/02 14:57:43 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 static int	ft_check_nl(char *str);
-static char	*free_foo(char *buffer, char *line);
+static char	*free_foo(char **buffer, char *line);
 static char	*ft_update(char *buffer);
 static char	*make_line(char **buffer, char *line, int code);
 
@@ -25,7 +25,7 @@ char	*get_next_line(int fd)
 
 	line = NULL;
 	if (fd < 0 || read(fd, 0, 0) < 0)
-		return (free_foo(buffer, NULL));
+		return (free_foo(&buffer, NULL));
 	bytes_read = 1;
 	while (!ft_check_nl(buffer) && bytes_read > 0)
 	{
@@ -34,7 +34,7 @@ char	*get_next_line(int fd)
 			return (NULL);
 		bytes_read = read(fd, buffer + ft_strlen(buffer), BUFFER_SIZE);
 		if (bytes_read < 0)
-			return (free_foo(buffer, NULL));
+			return (free_foo(&buffer, NULL));
 	}
 	if (ft_check_nl(buffer))
 		return (make_line(&buffer, line, 1));
@@ -61,11 +61,11 @@ static int	ft_check_nl(char *str)
 	return (0);
 }
 
-static char	*free_foo(char *buffer, char *line)
+static char	*free_foo(char **buffer, char *line)
 {
-	free(buffer);
+	free(*buffer);
 	free(line);
-	buffer = NULL;
+	*buffer = NULL;
 	return (NULL);
 }
 
@@ -84,14 +84,14 @@ static char	*ft_update(char *buffer)
 	{
 		tmp_buffer = ft_calloc(BUFFER_SIZE + 1 + ft_strlen(buffer), 1);
 		if (!tmp_buffer)
-			return (free_foo(buffer, NULL));
+			return (free_foo(&buffer, NULL));
 		ft_strlcpy(tmp_buffer, buffer, ft_strlen(buffer) + 1);
 		free(buffer);
 		return (tmp_buffer);
 	}
 	tmp_buffer = ft_calloc(ft_strlen(ft_strchr(buffer, '\n') + 1) + 1, 1);
 	if (!tmp_buffer)
-		return (free_foo(buffer, NULL));
+		return (free_foo(&buffer, NULL));
 	ft_strlcpy(tmp_buffer, (ft_strchr(buffer, '\n') + 1),
 		ft_strlen(ft_strchr(buffer, '\n') + 1) + 1);
 	free(buffer);
@@ -106,17 +106,17 @@ static char	*make_line(char **buffer, char *line, int code)
 		ft_strlcpy(line, *buffer, (ft_strchr(*buffer, '\n') + 2 - *buffer));
 		*buffer = ft_update(*buffer);
 		if (!*buffer || !line)
-			return (free_foo(*buffer, line));
+			return (free_foo(buffer, line));
 		return (line);
 	}
 	line = ft_calloc(1, ft_strlen(*buffer) + 1);
 	ft_strlcpy(line, *buffer, ft_strlen(*buffer) + 1);
 	*buffer = ft_update(*buffer);
 	if (!*buffer || !line)
-		return (free_foo(*buffer, line));
+		return (free_foo(buffer, line));
 	return (line);
 }
-/* 
+
 #include <stdio.h>
 
 int	main(void)
@@ -128,9 +128,13 @@ int	main(void)
 	str = get_next_line(fd);
 	printf("%s", str);
 	free(str);
+	close(fd);
+	fd = -1;
 	str = get_next_line(fd);
 	printf("%s", str);
 	free(str);
+	printf("\n");
+	fd = open("./texts/text.txt", O_RDWR);
 	str = get_next_line(fd);
 	printf("%s", str);
 	free(str);
@@ -142,4 +146,4 @@ int	main(void)
 	free(str);
 	close(fd);
 	return (0);
-} */
+}

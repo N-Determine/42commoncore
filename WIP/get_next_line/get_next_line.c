34195da5f0ad5942594fcd1bdff04a5348bc082c
Andreas Theodorus Deters <adeters@c3r7p3.42vienna.com>
2024-10-02 13:38:32 +0200

    got rid of everything that doesn't need to be there

--- WIP/get_next_line/get_next_line.c
+++ WIP/get_next_line/get_next_line.c
@@ -6,157 +6,16 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 13:12:06 by adeters           #+#    #+#             */
-/*   Updated: 2024/10/02 13:30:21 by adeters          ###   ########.fr       */
+/*   Updated: 2024/10/02 13:37:42 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <fcntl.h>
-#include <stdint.h>
-#include <stdio.h>
 
-int	ft_check_nl(char *str)
-{
-	int	index;
-
-	index = 0;
-	if (str == NULL)
-		return (0);
-	while (str[index])
-	{
-		if (str[index] == '\n')
-			return (1);
-		index++;
-	}
-	return (0);
-}
-
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
-{
-	size_t	i;
-	size_t	y;
-
-	i = 0;
-	y = 0;
-	if (!dest || !src)
-		return (0);
-	while (src[y])
-		y++;
-	if (size > 0)
-	{
-		while (i < (size - 1) && i < y)
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		dest[i] = '\0';
-	}
-	return (y);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	int	i;
-
-	i = 0;
-	c = c % 256;
-	while (s[i])
-	{
-		if (s[i] == c)
-			return ((char *)&s[i]);
-		i++;
-	}
-	if (s[i] == c)
-		return ((char *)&s[i]);
-	return (0);
-}
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (str == NULL)
-		return (0);
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_calloc(size_t nmemb, size_t size)
-{
-	char	*ptr;
-	int		i;
-
-	i = 0;
-	if (size != 0 && nmemb > SIZE_MAX / size)
-		return (NULL);
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-		return (NULL);
-	while (i < nmemb * size)
-	{
-		ptr[i] = '\0';
-		i++;
-	}
-	return (ptr);
-}
-
-char	*free_foo(char *buffer, char *line)
-{
-	free(buffer);
-	free(line);
-	buffer = NULL;
-	return (NULL);
-}
-
-char	*ft_update(char *buffer)
-{
-	char	*tmp_buffer;
-
-	if (buffer == NULL)
-	{
-		buffer = ft_calloc(BUFFER_SIZE + 1, 1);
-		if (!buffer)
-			return (NULL);
-		return (buffer);
-	}
-	else if (!(ft_check_nl(buffer)))
-	{
-		tmp_buffer = ft_calloc(BUFFER_SIZE + 1 + ft_strlen(buffer), 1);
-		if (!tmp_buffer)
-			return (free_foo(buffer, NULL));
-		ft_strlcpy(tmp_buffer, buffer, ft_strlen(buffer) + 1);
-		free(buffer);
-		return (tmp_buffer);
-	}
-	tmp_buffer = ft_calloc(ft_strlen(ft_strchr(buffer, '\n') + 1) + 1, 1);
-	if (!tmp_buffer)
-		return (free_foo(buffer, NULL));
-	ft_strlcpy(tmp_buffer, (ft_strchr(buffer, '\n') + 1),
-		ft_strlen(ft_strchr(buffer, '\n') + 1) + 1);
-	free(buffer);
-	return (tmp_buffer);
-}
-
-char	*make_line(char **buffer, char *line, int code)
-{
-	if (code == 1)
-	{
-		line = ft_calloc(1, (ft_strchr(*buffer, '\n') + 2 - *buffer) + 1);
-		ft_strlcpy(line, *buffer, (ft_strchr(*buffer, '\n') + 2 - *buffer));
-		*buffer = ft_update(*buffer);
-		if (!*buffer || !line)
-			return (free_foo(*buffer, line));
-		return (line);
-	}
-	line = ft_calloc(1, ft_strlen(*buffer) + 1);
-	ft_strlcpy(line, *buffer, ft_strlen(*buffer) + 1);
-	*buffer = ft_update(*buffer);
-	if (!*buffer || !line)
-		return (free_foo(*buffer, line));
-	return (line);
-}
+static int	ft_check_nl(char *str);
+static char	*free_foo(char *buffer, char *line);
+static char	*ft_update(char *buffer);
+static char	*make_line(char **buffer, char *line, int code);
 
 char	*get_next_line(int fd)
 {
@@ -186,6 +45,80 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
+static int	ft_check_nl(char *str)
+{
+	int	index;
+
+	index = 0;
+	if (str == NULL)
+		return (0);
+	while (str[index])
+	{
+		if (str[index] == '\n')
+			return (1);
+		index++;
+	}
+	return (0);
+}
+
+static char	*free_foo(char *buffer, char *line)
+{
+	free(buffer);
+	free(line);
+	buffer = NULL;
+	return (NULL);
+}
+
+static char	*ft_update(char *buffer)
+{
+	char	*tmp_buffer;
+
+	if (buffer == NULL)
+	{
+		buffer = ft_calloc(BUFFER_SIZE + 1, 1);
+		if (!buffer)
+			return (NULL);
+		return (buffer);
+	}
+	else if (!(ft_check_nl(buffer)))
+	{
+		tmp_buffer = ft_calloc(BUFFER_SIZE + 1 + ft_strlen(buffer), 1);
+		if (!tmp_buffer)
+			return (free_foo(buffer, NULL));
+		ft_strlcpy(tmp_buffer, buffer, ft_strlen(buffer) + 1);
+		free(buffer);
+		return (tmp_buffer);
+	}
+	tmp_buffer = ft_calloc(ft_strlen(ft_strchr(buffer, '\n') + 1) + 1, 1);
+	if (!tmp_buffer)
+		return (free_foo(buffer, NULL));
+	ft_strlcpy(tmp_buffer, (ft_strchr(buffer, '\n') + 1),
+		ft_strlen(ft_strchr(buffer, '\n') + 1) + 1);
+	free(buffer);
+	return (tmp_buffer);
+}
+
+static char	*make_line(char **buffer, char *line, int code)
+{
+	if (code == 1)
+	{
+		line = ft_calloc(1, (ft_strchr(*buffer, '\n') + 2 - *buffer) + 1);
+		ft_strlcpy(line, *buffer, (ft_strchr(*buffer, '\n') + 2 - *buffer));
+		*buffer = ft_update(*buffer);
+		if (!*buffer || !line)
+			return (free_foo(*buffer, line));
+		return (line);
+	}
+	line = ft_calloc(1, ft_strlen(*buffer) + 1);
+	ft_strlcpy(line, *buffer, ft_strlen(*buffer) + 1);
+	*buffer = ft_update(*buffer);
+	if (!*buffer || !line)
+		return (free_foo(*buffer, line));
+	return (line);
+}
+/* 
+#include <stdio.h>
+
 int	main(void)
 {
 	int fd = open("./texts/text.txt", O_RDWR);
@@ -209,4 +142,4 @@ int	main(void)
 	free(str);
 	close(fd);
 	return (0);
-}
+} */
--- WIP/get_next_line/get_next_line.h
+++ WIP/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 16:44:55 by adeters           #+#    #+#             */
-/*   Updated: 2024/10/02 13:15:00 by adeters          ###   ########.fr       */
+/*   Updated: 2024/10/02 13:37:31 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,13 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdint.h>
+
+int		ft_strlen(char *str);
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_calloc(size_t nmemb, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 
 #endif

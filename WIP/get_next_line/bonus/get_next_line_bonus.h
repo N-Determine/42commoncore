3b502b16f9298477254d677993e96499b225fecd
Andreas Theodorus Deters <adeters@c2r5p2.42vienna.com>
2024-10-09 20:20:20 +0200

    fixed header protection and deleted main

--- WIP/get_next_line/bonus/get_next_line_bonus.h
+++ WIP/get_next_line/bonus/get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 16:44:55 by adeters           #+#    #+#             */
-/*   Updated: 2024/10/02 13:37:31 by adeters          ###   ########.fr       */
+/*   Updated: 2024/10/09 20:19:59 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024

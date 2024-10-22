6ea901ffc991d1f77bf59bcae454e49a3f927928
Andreas Theodorus Deters <adeters@c2r5p2.42vienna.com>
2024-10-22 18:33:14 +0200

    changed folder structure

---
+++ Other Files/Side quests/TicTacToe/tictactoe.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void print_game_state(char *game_state)
+{
+	while 
+}
+
+int	main(void)
+{
+	char *game_state = "X0X--X0-X";
+	print_game_state(game_state);
+	return (0);
+}
+
+
+##################################################
+##################################################
+##
+##
+##
+##
+##
+##
+##
+##
+##
+##
+##
+##
+##
+##

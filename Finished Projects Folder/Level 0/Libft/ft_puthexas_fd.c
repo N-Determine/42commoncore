5a49afabaee52a53e9b5aba9c7725a91b2c3a953
N-Determine <sreted@outlook.com>
2024-11-15 19:07:31 +0100

    got rid of shit

---
+++ Finished Projects Folder/Level 0/Libft/ft_puthexas_fd.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthexas_fd.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 14:30:43 by adeters           #+#    #+#             */
+/*   Updated: 2024/09/18 17:58:37 by adeters          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include "libft.h"
+
+int	ft_puthexas_fd(char code, va_list list, int fd)
+{
+	long long	vptr;
+
+	if (code == 'p')
+	{
+		vptr = (long long)va_arg(list, void *);
+		if (vptr != 0)
+		{
+			ft_puthexaident_fd('x', fd);
+			return (ft_putnbr_base_fd(vptr, "0123456789abcdef", fd) + 2);
+		}
+		else
+		{
+			ft_putstr_fd("(nil)", fd);
+			return (5);
+		}
+	}
+	else if (code == 'x')
+		return (ft_putnbr_base_fd(va_arg(list, unsigned int),
+				"0123456789abcdef", fd));
+	else if (code == 'X')
+		return (ft_putnbr_base_fd(va_arg(list, unsigned int),
+				"0123456789ABCDEF", fd));
+	return (-1);
+}
---
+++ Finished Projects Folder/Level 0/Libft/main.c
@@ -0,0 +1,6 @@
+#include "libft.h"
+
+int main(void)
+{
+	ft_printf("Hello World %i\n", 5);
+}

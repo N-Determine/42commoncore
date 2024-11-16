f096809f29c66062effb7aa8b6619e1900342a57
N-Determine <sreted@outlook.com>
2024-11-16 19:31:32 +0100

    added a struct for the stacks

--- WIP/push_swap/push_swap.c
+++ WIP/push_swap/push_swap.c
@@ -6,16 +6,25 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 20:38:40 by adeters           #+#    #+#             */
-/*   Updated: 2024/11/16 19:14:09 by adeters          ###   ########.fr       */
+/*   Updated: 2024/11/16 19:31:23 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
+// Initialize stack b as empty thing
+// In a loop from 1 to ac
+	// create a new node from a t_dlist with ft_atoi(av[i]) as content
+int fill_stacks(int ac, char **av, t_stacks *stacks)
+{
+	return (1);
+}
+
 int	main(int ac, char **av)
 {
-	t_fails	fails;
+	t_fails		fails;
+	t_stacks	stacks;
 
 	if (ac < 2)
 		return (print_errors_args(USAGE, &fails), 1);

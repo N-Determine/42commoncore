ff22130d104a9a1dcaf09d4f17e7d08acfdbb204
N-Determine <sreted@outlook.com>
2024-11-16 19:05:24 +0100

    added functionality to detect duplicates in the input ranges

--- WIP/push_swap/check_args.c
+++ WIP/push_swap/check_args.c
@@ -6,11 +6,55 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 18:42:44 by adeters           #+#    #+#             */
-/*   Updated: 2024/11/16 18:48:26 by adeters          ###   ########.fr       */
+/*   Updated: 2024/11/16 19:04:55 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	if (!s1 || !s2)
+		return (0);
+	i = 0;
+	while (s1[i])
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	while (s2[i])
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	check_duplicates(int ac, char **av, t_fails *fails)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < ac)
+	{
+		j = i + 1;
+		while (j < ac)
+		{
+			if (ft_strcmp(av[i], av[j]) == 0)
+				fails->duplicates++;
+			j++;
+		}
+		i++;
+	}
+	if (fails->duplicates > 0)
+		return (1);
+	return (0);
+}
 
 int	check_args(int ac, char **av, t_fails *fails)
 {
@@ -18,7 +62,7 @@ int	check_args(int ac, char **av, t_fails *fails)
 	int	current;
 
 	i = 1;
-	init_fails(&fails);
+	init_fails(fails);
 	while (i < ac)
 	{
 		current = check_overflow(av[i]);
@@ -30,6 +74,8 @@ int	check_args(int ac, char **av, t_fails *fails)
 			fails->formats++;
 		i++;
 	}
+	if (check_duplicates(ac, av, fails))
+		return (1);
 	if (fails->overflows > 0 || fails->underflows > 0)
 		return (1);
 	if (fails->formats > 0)

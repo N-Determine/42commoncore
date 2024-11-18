4f74bb7dff1b3fdc0c5ec01d89f986076bfad636
N-Determine <sreted@outlook.com>
2024-11-18 18:37:35 +0100

    improved tester.py documentation

--- WIP/push_swap/tester.py
+++ WIP/push_swap/tester.py
@@ -2,17 +2,17 @@ import random
 import subprocess
 import sys
 
+# Make sure that the right amount of arguments have been passed
 if len(sys.argv) != 2:
-	print("Insert the amount of numbers as command-line argument")
+	print("Usage: python3 tester.py [Amount of numbers]")
 	exit(1)
 numbers = int(sys.argv[1])
 
-# Generate 100 unique random numbers
+# Generate unique random numbers
 random_numbers = random.sample(range(-2147483648, 2147483647), numbers)
 random_numbers_str = list(map(str, random_numbers))  # Convert numbers to strings
 
-# Path to your program (replace 'your_program' with the actual path or command)
-program_path = "./push_swap"  # Ensure the program is executable
+program_path = "./push_swap"
 
 # Combine the program path with the arguments
 command = [program_path] + random_numbers_str

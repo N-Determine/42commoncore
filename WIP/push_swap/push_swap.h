36c4599f32b24c0aa8e5adaefc372492111bbc53
N-Determine <sreted@outlook.com>
2024-11-30 16:44:02 +0100

    added more doxygen documentation for the missing functions

--- WIP/push_swap/push_swap.h
+++ WIP/push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 20:47:24 by adeters           #+#    #+#             */
-/*   Updated: 2024/11/29 16:40:28 by adeters          ###   ########.fr       */
+/*   Updated: 2024/11/30 16:43:15 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -167,12 +167,6 @@ typedef struct s_stacks
 }					t_stacks;
 
 // FUNCTIONS
-// push_swap.c
-/**
- * @brief Chooses the right algorithm based on the amount of numbers
- * that need to be sorted
- */
-int					push_swap(t_stacks *stacks);
 // block_sort.c
 int					block_sort(t_stacks *stacks, int divider);
 // check_args.c
@@ -241,28 +235,128 @@ int					check_args(int ac, char **av, t_fails *fails);
  * characters, or no digits after `+` or `-`).
  */
 int					check_overflow(char *str);
-// print_ops.c
+// direction.c
 /**
- * @brief Prints an operation to the stdout
+ * @brief Checks if a number is within a specified range.
+ *
+ * This function determines if a given number `nb` falls inclusively
+ * within the range defined by `start` and `end`.
+ *
+ * @param nb The number to check.
+ * @param start The start of the range (inclusive).
+ * @param end The end of the range (inclusive).
+ * @return 1 if `nb` is within the range `[start, end]`, otherwise 0.
  */
-void				print_ops(int operation);
-// print_errors.c
+int					in_ran(int nb, int start, int end);
 /**
- * @brief Prints errors to the stderr. It also takes the t_fails
- * struct as an imput what allows it to print values in combination with
- * the errors (e.g. Amount of integer overflows etc.)
+ * @brief Finds the position of a number in a doubly linked list.
+ *
+ * This function iterates through a doubly linked list starting from `head` 
+ * and counts the number of steps required to find the first occurrence of 
+ * the given number `n`. If the number is not found, the total number of 
+ * nodes in the list is returned.
+ *
+ * @param head A pointer to the head of the doubly linked list.
+ * @param n The number to find in the list.
+ * @return The number of steps (nodes) to reach the first occurrence of `n`. 
+ *         If `n` is not found, the total node count is returned.
  */
-void				print_errors_args(int code, t_fails *fails);
+int					nb_pos_down(t_dlist *head, int n);
 /**
- * @brief Prints errors to the stderr
+ * @brief Finds the position of a number in a doubly linked list, 
+ * starting from the tail.
+ *
+ * This function iterates through a doubly linked list starting from the last
+ * node and counts the number of steps required to find the first 
+ * occurrence of the given number `n`. If the number is not found, 
+ * the total number of nodes in the list is returned.
+ *
+ * @param head A pointer to the head of the doubly linked list.
+ * @param n The number to find in the list.
+ * @return The number of steps (nodes) to reach the first occurrence of `n`. 
+ *         If `n` is not found, the total node count is returned.
  */
-void				print_errors(int code);
-// init.c
+int					nb_pos_up(t_dlist *head, int n);
 /**
- * @brief Initializes the t_fails struct so that functions that count
- * the amount of errors work properly (by setting errors counts to 0)
+ * @brief Finds the position of a number within a range in a doubly linked list.
+ *
+ * This function iterates through a doubly linked list starting from `head` 
+ * and counts the number of steps required to find the first occurrence of 
+ * a number within the range `[start, end]`. If no number in the range is found,
+ * the total number of nodes in the list is returned.
+ *
+ * @param head A pointer to the head of the doubly linked list.
+ * @param start The start of the range (inclusive).
+ * @param end The end of the range (inclusive).
+ * @return The number of steps (nodes) to reach the first occurrence of a number 
+ *         in the range `[start, end]`. If no number in the range is found, 
+ *         the total node count is returned.
  */
-void				init_fails(t_fails *fails);
+int					nb_pos_down_range(t_dlist *head, int start, int end);
+/**
+ * @brief Finds the position of a number within a range in a doubly linked list,
+ * starting from the tail.
+ *
+ * This function iterates through a doubly linked list starting from the last
+ * node and counts the number of steps required to find the first occurrence of
+ * a number within the range `[start, end]`. If no number in the range is found,
+ * the total number of nodes in the list is returned.
+ *
+ * @param head A pointer to the head of the doubly linked list.
+ * @param start The start of the range (inclusive).
+ * @param end The end of the range (inclusive).
+ * @return The number of steps (nodes) to reach the first occurrence of a number 
+ *         in the range `[start, end]`. If no number in the range is found, 
+ *         the total node count is returned.
+ */
+int					nb_pos_up_range(t_dlist *head, int start, int end);
+// dlist.c
+/**
+ * @brief Finds the last node in a doubly linked list.
+ *
+ * This function traverses a doubly linked list starting from `head` 
+ * and returns a pointer to the last node in the list. If the list is empty,
+ * the function returns `NULL`.
+ *
+ * @param head A pointer to the head of the doubly linked list.
+ * @return A pointer to the last node in the list. If the list is empty, 
+ * `NULL` is returned.
+ */
+t_dlist				*last_node(t_dlist *head);
+/**
+ * @brief Checks if a doubly linked list is sorted in ascending order.
+ *
+ * This function iterates through a doubly linked list starting from `head` 
+ * and checks whether all the elements are sorted in ascending order. 
+ * If the list is empty or sorted, the function returns `1`. Otherwise, 
+ * it returns `0`.
+ *
+ * @param head A pointer to the head of the doubly linked list.
+ * @return `1` if the list is empty or sorted in ascending order, otherwise `0`.
+ */
+int					is_sorted(t_dlist *head);
+/**
+ * @brief Finds the largest number in a doubly linked list.
+ *
+ * This function iterates through a doubly linked list starting from `head` 
+ * and finds the largest number among the nodes. If the list is empty, 
+ * the function returns `0`.
+ *
+ * @param head A pointer to the head of the doubly linked list.
+ * @return The largest number in the list. If the list is empty, returns `0`.
+ */
+int					find_biggest(t_dlist *head);
+/**
+ * @brief Finds the smallest number in a doubly linked list.
+ *
+ * This function iterates through a doubly linked list starting from `head` 
+ * and finds the smallest number among the nodes. If the list is empty, 
+ * the function returns `0`.
+ *
+ * @param head A pointer to the head of the doubly linked list.
+ * @return The smallest number in the list. If the list is empty, returns `0`.
+ */
+int					find_smallest(t_dlist *head);
 // filling.c
 /**
  * Allocates space for an array and a t_dlist struct to be filled
@@ -275,6 +369,85 @@ int					fill_stacks(int ac, char **av, t_stacks *stacks,
  * nodes_stack_a counter in the t_stacks struct.
  */
 void				clear_stack_a(t_stacks *stacks);
+// init.c
+/**
+ * @brief Initializes the t_fails struct so that functions that count
+ * the amount of errors work properly (by setting errors counts to 0)
+ */
+void				init_fails(t_fails *fails);
+// main_helpers.c
+/**
+ * @brief Checks if a string represents a valid number.
+ *
+ * This function iterates through each character of the input string `str`
+ * and checks whether each character is a digit, or a plus (`+`) 
+ * or minus (`-`) sign. If all characters are valid, it returns `1`. 
+ * If any character is invalid, it returns `0`.
+ *
+ * @param str A pointer to the string to check.
+ * @return `1` if the string represents a valid number, otherwise `0`.
+ */
+int					ft_isdigit_str(char *str);
+/**
+ * @brief Counts the number of elements in the modified `char **av`
+ * from the input. Basically updates `int ac` in case that the 
+ * values were given as a single string instead of individual
+ * command-line arguments.
+ *
+ * @param arr A pointer to the array of strings.
+ * @return The number of elements in the array (excluding `NULL`).
+ */
+int					new_count(char **arr);
+/**
+ * @brief Frees all dynamically allocated memory for the array and stack.
+ *
+ * This function frees each string in the array `arr` (up to `stacks->len`), 
+ * then frees the array itself. It only performs these actions if the `str_in` 
+ * field in `stacks` is not `NULL` and if `stacks->len` is greater than zero.
+ *
+ * @param arr A pointer to the array of strings to be freed.
+ * @param stacks A pointer to the `t_stacks` structure that contains 
+ * information about the array length.
+ */
+void				ft_free_all(char **arr, t_stacks *stacks);
+/**
+ * @brief Checks if a single command-line argument is a valid number.
+ *
+ * This function verifies if a single input provided as a command-line argument
+ * can be considered a valid number, allowing for optional leading zeros, 
+ * and a plus (`+`) or minus (`-`) sign. If the input is invalid, it prints 
+ * an appropriate error message. If the input is valid, the function 
+ * returns `0`,indicating successful validation and completion.
+ *
+ * @param ac The argument count passed to the program.
+ * @param av The argument vector containing the command-line arguments.
+ * @param fails A pointer to the `t_fails` 
+ * structure that stores error information.
+ * @return `1` if the input is invalid
+ * 
+ * `0` if the input is valid.
+ */
+int					check_single_str(int ac, char **av, t_fails *fails);
+/**
+ * @brief Converts a single input string into an array of command-line arguments.
+ *
+ * This function takes a single string input (e.g., a series of values separated 
+ * by spaces) and splits it into an array of strings, mimicking the command-line 
+ * argument format. The resulting array is used by the program as if the values 
+ * were given as individual command-line arguments. It also updates the argument 
+ * count (`ac`) using the `new_count()` fuction and stores relevant information 
+ * in the `stacks` structure. If an error occurs during splitting or 
+ * memory allocation, an error message is printed.
+ *
+ * @param input The input string containing values separated by spaces.
+ * @param ac A pointer to the argument count, which will be 
+ * updated with the number of elements.
+ * @param stacks A pointer to the `t_stacks` structure, where the 
+ * length of the new array and status are stored.
+ * @return A pointer to the newly created array of strings 
+ * (command-line arguments), or `NULL` if an error occurs.
+ */
+char				**new_av_maker(char *input, int *ac, t_stacks *stacks);
 // ops_push.c
 /**
  * @brief	Takes the first element at the top of `stack_b` and puts
@@ -385,40 +558,27 @@ void				swap_b(t_stacks *stacks, int code);
  * @note Always prints the operation to the stdout.
  */
 void				swap_s(t_stacks *stacks);
-// dlist.c
+// print_errors.c
 /**
- * @brief Iterates through a `t_dlist` and returns the last
- * node.
+ * @brief Prints errors to the stderr. It also takes the t_fails
+ * struct as an imput what allows it to print values in combination with
+ * the errors (e.g. Amount of integer overflows etc.)
  */
-t_dlist				*last_node(t_dlist *head);
-int					find_biggest(t_dlist *head);
-int					find_smallest(t_dlist *head);
+void				print_errors_args(int code, t_fails *fails);
 /**
- * @brief Checks if a `t_dlist` is sorted in non-descending
- * order
- *
- * @return The function returns:
- *
- * - `0` if the list is not sorted
- *
- * - `1` if the list is sorted or if the list is equal to `NULL`
+ * @brief Prints errors to the stderr
  */
-int					is_sorted(t_dlist *head);
-// direction.c
+void				print_errors(int code);
+// print_ops.c
 /**
- * @brief Checks if a number nb is within the range of int start
- * and int end (inklusive)
+ * @brief Prints an operation to the stdout
  */
-int					in_ran(int nb, int start, int end);
-int					nb_pos_down(t_dlist *head, int n);
-int					nb_pos_up(t_dlist *head, int n);
-int					nb_pos_down_range(t_dlist *head, int start, int end);
-int					nb_pos_up_range(t_dlist *head, int start, int end);
-// main_helpers.c
-int					ft_isdigit_str(char *str);
-int					new_count(char **arr);
-void				ft_free_all(char **arr, t_stacks *stacks);
-int					check_single_str(int ac, char **av, t_fails *fails);
-char				**new_av_maker(char *input, int *ac, t_stacks *stacks);
+void				print_ops(int operation);
+// push_swap.c
+/**
+ * @brief Chooses the right algorithm based on the amount of numbers
+ * that need to be sorted
+ */
+int					push_swap(t_stacks *stacks);
 
 #endif

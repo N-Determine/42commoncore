f27109e516b9b69a2a03571fe3a1baa8bd3de0f2
N-Determine <sreted@outlook.com>
2024-11-30 17:15:56 +0100

    did a check

--- WIP/push_swap/main.c
+++ WIP/push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 20:38:40 by adeters           #+#    #+#             */
-/*   Updated: 2024/11/30 17:04:24 by adeters          ###   ########.fr       */
+/*   Updated: 2024/11/30 17:14:15 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ int	main(int ac, char **av)
 		return (print_errors(USAGE), 1);
 	if (ac == 2 && ft_isdigit_str(av[1]))
 		return (check_single_str(ac, av, &fails));
-	else if (ac == 2 && str_splitable(av[1]))
+	else if (ac == 2 && !str_splitable(av[1]))
 		return (print_errors(SPLIT), 1);
 	else if (ac == 2)
 		av = new_av_maker(av[1], &ac, &stacks);
 	if (!av)
-		return (1);
+		return (print_errors(MAL), 1);
 	if (check_args(ac, av, &fails))
 		return (ft_free_all(av, &stacks), print_errors_args(ARGS, &fails), 1);
 	if (fill_stacks(ac, av, &stacks, &fails))
@@ -49,14 +49,14 @@ int	main(int ac, char **av)
  * This function checks if a given string contains only characters that can 
  * be part of a valid number (digits, plus (`+`) or minus (`-`) signs,
  * and spaces). 
- * If the string contains any other characters, the function returns `1`, 
+ * If the string contains any other characters, the function returns `0`, 
  * indicating the string is invalid for splitting. If the string only contains 
- * valid characters, it returns `0`.
+ * valid characters, it returns `1`.
  *
  * @param str The string to check.
- * @return `1` if the string contains invalid characters 
+ * @return `0` if the string contains invalid characters 
  * 
- * `0` if it only contains valid characters.
+ * `1` if it only contains valid characters.
  */
 int	str_splitable(char *str)
 {
@@ -67,8 +67,8 @@ int	str_splitable(char *str)
 	{
 		if (!ft_isdigit(str[i]) && str[i] != ' ' && str[i] != '-'
 			&& str[i] != '+')
-			return (1);
+			return (0);
 		i++;
 	}
-	return (0);
+	return (1);
 }

7a6cb89185c380f1ecb999ee6c160326a7879e57
N-Determine <sreted@outlook.com>
2024-12-16 16:27:56 +0100

    added function that allocates and concats either 2 or 3 strings

--- WIP/pipex/main.c
+++ WIP/pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 13:43:35 by adeters           #+#    #+#             */
-/*   Updated: 2024/12/16 16:10:37 by adeters          ###   ########.fr       */
+/*   Updated: 2024/12/16 16:27:18 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,59 @@ char	**get_paths(char **env)
 	return (paths);
 }
 
+char	*allo_strcat(char *str1, char *str2)
+{
+	int		size;
+	char	*res;
+	int		i;
+	int		j;
+
+	if (!str1 | !str2)
+		return (NULL);
+	size = ft_strlen(str1) + ft_strlen(str2) + 1;
+	res = ft_calloc(size, sizeof(char));
+	if (!res)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (str1[i])
+	{
+		res[i] = str1[i];
+		i++;
+	}
+	while (str2[j])
+	{
+		res[i] = str2[j];
+		i++;
+		j++;
+	}
+	return (res);
+}
+
+char	*allo_trip_strcat(char *str1, char *str2, char *str3)
+{
+	char	*tmp;
+	char	*res;
+
+	if (!str1 | !str2 | !str3)
+		return (NULL);
+	tmp = allo_strcat(str1, str2);
+	if (!tmp)
+		return (NULL);
+	res = allo_strcat(tmp, str3);
+	if (!res)
+		return (free(tmp), NULL);
+	return (free(tmp), res);
+}
+
 int	main(int ac, char **av, char **env)
 {
 	char **paths = get_paths(env);
+	if (!paths)
+		return (1);
+	char *first = allo_trip_strcat(paths[0], "/", "ls");
+	ft_printf("first path: %s\n", first);
+	free(first);
 	ft_free_list(paths);
 	return (0);
 }

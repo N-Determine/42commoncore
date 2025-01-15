f8d67e9587b31e53b0fe98a7cdf19260ef6ae4a6
N-Determine <sreted@outlook.com>
2025-01-15 18:46:32 +0100

    added init_prog function to philosophers

--- WIP/philosophers/src/main.c
+++ WIP/philosophers/src/main.c
@@ -6,17 +6,24 @@
 /*   By: adeters <adeters@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:03:52 by adeters           #+#    #+#             */
-/*   Updated: 2025/01/15 18:41:03 by adeters          ###   ########.fr       */
+/*   Updated: 2025/01/15 18:46:01 by adeters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+int	init_prog(t_data *data)
+{
+	gettimeofday(&data->start, NULL);
+	return (0);
+}
+
 int	main(void)
 {
 	t_data	data;
 
-	gettimeofday(&data.start, NULL);
+	if (init_prog(&data))
+		return (1);
 	usleep(213 * 1000);
 	p_log(&data, 5, FORK);
 }
